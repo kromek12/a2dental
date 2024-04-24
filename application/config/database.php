032ab279a6a8014769eaba016b66c37acf73b738
kromek12 <oscararroyo423@gmail.com>
2024-04-23 22:47:09 -0600

    se anexan conexion a base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -7,9 +7,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '12345678',
+	'hostname' => '162.241.20.140',
+	'username' => 'sublim21_oscar',
+	'password' => 'Kromek12.',
 	'database' => 'a2dental',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
